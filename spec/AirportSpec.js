3dfc782547a067051c07ba296b6cd5b7fe3944ce
Laurenlau <harris_lau@hotmail.co.uk>
2019-08-22 21:37:26 +0100

    Planes can now takeoff from the airport

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -11,9 +11,15 @@ describe('Airport', function() {
   it('has no planes by default', function() {
     expect(airport.planes()).toEqual([]);
   });
-  
+
   it('can clear planes for landing', function() {
     airport.clearForLanding(plane);
     expect(airport.planes()).toEqual([plane]);
+  });
+
+  it('can clear planes for takeoff', function(){
+    airport.clearForLanding(plane);
+    airport.clearForTakeOff(plane);
+    expect(airport.planes()).toEqual([]);
   });
 });
